325432856ef4a8668b484b8af4929418e35d43b1
Swetha27812 <vtu26633@veltech.edu.in>
2026-04-09 11:21:31 +0530

    Add files via upload

---
+++ Week-2/TimeConversion.java
@@ -0,0 +1,53 @@
+package Tasks;
+import java.util.Scanner;
+
+public class TimeConversion {
+	
+
+	
+
+	    /**
+	     * Converts 12-hour AM/PM format to 24-hour military time.
+	     * Input Format: hh:mm:ssAM or hh:mm:ssPM
+	     */
+	    public static String timeConversion(String s) {
+	        
+	        String period = s.substring(8, 10);
+	        
+	       
+	        int hour = Integer.parseInt(s.substring(0, 2));
+	        
+	      
+	        String minuteSecond = s.substring(2, 8);
+
+	        if (period.equalsIgnoreCase("AM")) {
+	           
+	            if (hour == 12) {
+	                hour = 0;
+	            }
+	        } else { 
+	           
+	            if (hour != 12) {
+	                hour += 12;
+	            }
+	        }
+
+	        
+	        return String.format("%02d%s", hour, minuteSecond);
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        
+	        System.out.print("Enter time (hh:mm:ssAM/PM): ");
+	        if (scan.hasNextLine()) {
+	            String input = scan.nextLine();
+	            String result = timeConversion(input);
+	            System.out.println("Military Time: " + result);
+	        }
+	        
+	        scan.close();
+	    }
+	}
+
+
